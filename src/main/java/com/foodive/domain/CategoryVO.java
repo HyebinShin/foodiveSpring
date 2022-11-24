2a3dc867c4e3fa466604eccbed3650d573f03ab5
HyebinShin <hyeviin.dev@gmail.com>
2022-11-24 19:44:16 +0900

    Feat: CategoryVO

---
+++ src/main/java/com/foodive/domain/CategoryVO.java
@@ -0,0 +1,17 @@
+package com.foodive.domain;
+
+import lombok.Data;
+
+import java.util.Date;
+
+@Data
+public class CategoryVO {
+    private Long cno;
+    private String code;
+    private String name;
+    private Integer state;
+
+    private Date regDate;
+    private Date modDate;
+    private Date dropDate;
+}
