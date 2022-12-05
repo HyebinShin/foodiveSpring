b392d7cb5d124c62b027afd6298ec5234c1a7cfc
HyebinShin <hyeviin.dev@gmail.com>
2022-12-05 20:31:18 +0900

    Test: 주문서 DB 등록 TEST
    주문/주문상세/배송/결제방법 DB에 등록
    주문 상품 목록 장바구니에서 제거(세션,DB 모두)
    주문 완료 후 해당 상품 재고 감소

--- src/test/java/com/foodive/service/OrderServiceTests.java
+++ src/test/java/com/foodive/service/OrderServiceTests.java
@@ -8,7 +8,10 @@ import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.web.context.request.RequestContextHolder;
+import org.springframework.web.context.request.ServletRequestAttributes;
 
+import javax.servlet.http.HttpSession;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
